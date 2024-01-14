46059c726ac934089a10398dbc7004c7a6da6561
Matteo0310 <145265011+Matteo0310@users.noreply.github.com>
2024-01-14 17:57:59 +0000

    feat: domain generator done.

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateDomain();
+  document.querySelector("#domain").textContent = generateDomain();
+  document.querySelector("#button");
+  button.addEventListener("click", () => window.onload());
 };
+function generateDomain() {
+  let articles = ["the", "our", "his"];
+  let adjectives = ["great", "big", "amazing"];
+  let nouns = ["jogger", "racoon", "dog", "merchant", "driver"];
+  let tlds = [".com", ".org", ".dog", ".cat", ".es"];
+
+  let articlesindex = Math.floor(Math.random() * articles.length);
+  let adjectivesindex = Math.floor(Math.random() * adjectives.length);
+  let nounsindex = Math.floor(Math.random() * nouns.length);
+  let tldsindex = Math.floor(Math.random() * tlds.length);
+
+  return (
+    articles[articlesindex] +
+    " " +
+    adjectives[adjectivesindex] +
+    " " +
+    nouns[nounsindex] +
+    " " +
+    tlds[tldsindex]
+  );
+}
